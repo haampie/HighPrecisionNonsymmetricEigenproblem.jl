ec41505c4f19c2dab3149a311e2ecafb6568b848
Harmen Stoppels <harmenstoppels@gmail.com>
2020-12-29 00:29:54 +0100

    Compute tsqr and only then apply it left and right

--- src/tsqr.jl
+++ src/tsqr.jl
@@ -3,126 +3,343 @@ using Base: OneTo
 using Base.Threads: @spawn
 using Base: @sync
 
-function to_band!(A::AbstractMatrix{T}, block_size = 32) where {T}
-    # iterate in blocks.
+struct Panel
+    w::Int
+    h::Int
+end
 
+raw"""
+Computes the amount of τs we have to store for all
+reflectors (I - τvv') in a tsqr. If we have 2 stacked panels
+of width w, we need 3 small qr's using w reflectors each, so
+3w τs.
+x   x
+ \ /
+  x
+
+Another example is 5 stacked panels, will go like this:
+x   x   x   x   x
+ \ /     \ /   /
+  x       x   /
+   \     /   /
+    \   /   /
+     \ /   /
+      x   /
+       \ /
+        x
+It's effectively 9 small qr's with w reflectors, so 9w τs.
+Generaly the formula is simply panel_width * (2 * num_panels_vertically - 1)
+"""
+function reflectors_per_tsqr(matrix_height, p::Panel)
+    num_panels_vertically = ÷(matrix_height, p.h, RoundUp)
+    reflectors_per_panel = p.w #assumes p.h ≥ p.w
+    return reflectors_per_panel * (2 * num_panels_vertically - 1)
+end
+
+function to_band!(A::AbstractMatrix{T}, p = Panel(16, 32)) where {T}
+    @assert size(A, 1) == size(A, 2)
+    @assert p.w ≤ p.h
+
+    # on top we have a section of size p.w x p.w
     m = size(A, 1)
-    blocks = ÷(m, block_size, RoundUp)
-  
-    offset_col = 1
-    offset_row = 1 + block_size
 
-    τs = Vector{T}(undef, m)
+    τs = Vector{T}(undef, reflectors_per_tsqr(m - p.w, p))
 
-    for block = OneTo(blocks)
-        cols_to_band!(A, offset_row, offset_col, block_size, τs)
-        offset_row += block_size
-        offset_col += block_size
+    # loop over all column blocks
+    for b = OneTo((m - p.w) ÷ p.w)
+
+        # divide into the bit we're QR'ing
+        # the bit to which we apply it from the left
+        # and the bit to which we apply it from the right.
+        curr_block = (b - 1) * p.w + 1
+        next_block = curr_block + p.w
+
+        A_qr = view(A, next_block:m, curr_block:next_block-1)
+
+        tsqr!(A_qr, τs, p)
+
+        # Now apply the tsqr bit from the left and the right to A.
+        A_left = view(A, next_block:m, next_block:m)
+        A_right = view(A, :, next_block:m)
+
+        apply_tsqr_left!(A_qr, A_left, τs, p)
+        apply_tsqr_right!(A_qr, A_right, τs, p)
     end
 
     return A
 end
 
-function cols_to_band!(A, offset_row, offset_col, block_size, τs)
-    # The part in which we do a tall and skinny QR
-    A_qr = view(A, offset_row:size(A, 1), offset_col:min(size(A, 2), offset_col+block_size-1))
-
-    # The part to which we apply the tall and skinny QR from the left
-    A_left = view(A, offset_row:size(A, 1), offset_col+block_size:size(A, 2))
-
-    # The part to which we apply the tall and skinny QR from the right.
-    A_right = view(A, :, offset_row:size(A, 1))
-
-    #=@sync=# for block_start = 1:block_size:size(A_qr, 1)
-        let
-            block_end = min(block_start + block_size - 1, size(A_qr, 1))
-            range = block_start:block_end
-            A_qr′ = view(A_qr, range, :)
-            τs′ = view(τs, range)
-            #=@spawn=# block_qr!(A_qr′, τs′)
-        end
+function exponent_of_2_greater_than_or_equal_to(m)
+    k = 0
+    two_to_the_k = 1
+    while two_to_the_k < m
+        two_to_the_k *= 2
+        k += 1
     end
 
-    #=@sync=# for block_start = 1:block_size:size(A_qr, 1)
-        let
-            block_end = min(block_start + block_size - 1, size(A_qr, 1))
-            range = block_start:block_end
-            A_qr′ = view(A_qr, range, :)
-            A_left′ = view(A_left, range, :)
-            τs′ = view(τs, range)
-            #=@spawn=# apply_left!(A_qr′, A_left′, τs′)
-        end
+    return k
+end
+
+function tsqr!(A, τs, p::Panel)
+    m, n = size(A)
+
+    @assert n == p.w
+
+    num_panels_vertically = ÷(m, p.h, RoundUp)
+
+    # Keep the τ offset conveniently here, cause
+    # we need that when walking up the tree later.
+    τ_offset = 1
+
+    # First standard QR on every panel.
+    for b = OneTo(num_panels_vertically)
+        b_start = (b - 1) * p.h + 1
+        b_end = min(b_start + p.h - 1, m)
+        τs′ = view(τs, τ_offset:τ_offset+p.w-1)
+        A′ = view(A, b_start:b_end, :)
+
+        block_qr!(A′, τs′)
+
+        τ_offset += p.w
     end
 
-    #=@sync=# for block_start = 1:block_size:size(A_qr, 1)
-        let
-            block_end = min(block_start + block_size - 1, size(A_qr, 1))
-            range = block_start:block_end
-            A_qr′ = view(A_qr, range, :)
-            A_right′ = view(A_right, :, range)
-            τs′ = view(τs, range)
-            #=@spawn=# apply_right!(A_qr′, A_right′, τs′)
-        end
-    end
+    # Then we walk up the tree and do QR on upper triangular blocks.
+    tree_height = exponent_of_2_greater_than_or_equal_to(num_panels_vertically)
 
-    # Pair-wise reduce to single QR.
-    merged_block_size = block_size
-    while true
-        # If there's only one block left, stop.
-        nblocks = ÷(size(A_qr, 1), merged_block_size, RoundUp)
-        nblocks ≤ 1 && break
+    # we're combining two panels of height `merged_block_size`
+    merged_block_size = p.h
 
-        #=@sync=# for block_start = 1:2merged_block_size:size(A_qr, 1)
-            let
-                # If only a single block or less fits, stop.
-                block_start + merged_block_size - 1 ≥ size(A_qr, 1) && break
-    
-                block_end = min(block_start + 2merged_block_size - 1, size(A_qr, 1))
-                range = block_start:block_end
-                A_qr′ = view(A_qr, range, :)
-                τs′ = view(τs, range)
-                #=@spawn=# merge_block_qr!(A_qr′, τs′, merged_block_size)
-            end
-        end
+    # we've already done the leaf nodes, so - 1.
+    for level = OneTo(tree_height)
+        num_panels_vertically = ÷(m, merged_block_size, RoundUp)
 
-        #=@sync=# for block_start = 1:2merged_block_size:size(A_qr, 1)
-            let
-                # If only a single block or less fits, stop.
-                block_start + merged_block_size - 1 ≥ size(A_qr, 1) && break
+        # combine all pairs of panels.
+        for b = OneTo(num_panels_vertically ÷ 2)
+            b_start = (b - 1) * 2merged_block_size + 1
+            b_end = min(b_start + 2merged_block_size - 1, m)
 
-                block_end = min(block_start + 2merged_block_size - 1, size(A_qr, 1))
-                range = block_start:block_end
-                A_qr′ = view(A_qr, range, :)
-                A_left′ = view(A_left, range, :)
-                τs′ = view(τs, range)
-                #=@spawn=# apply_left_with_gap!(A_qr′, A_left′, τs′, merged_block_size)
-            end
-        end
-
-        #=@sync=# for block_start = 1:2merged_block_size:size(A_qr, 1)
-            let
-                # If only a single block or less fits, stop.
-                block_start + merged_block_size - 1 ≥ size(A_qr, 1) && break
-
-                block_end = min(block_start + 2merged_block_size - 1, size(A_qr, 1))
-                range = block_start:block_end
-                A_qr′ = view(A_qr, range, :)
-                A_right′ = view(A_right, :, range)
-                τs′ = view(τs, range)
-                #=@spawn=# apply_right_with_gap!(A_qr′, A_right′, τs′, merged_block_size)
-            end
+            A′ = view(A, b_start:b_end, :)
+            τs′ = view(τs, τ_offset:τ_offset+p.w-1)
+            
+            #=@spawn=# merge_block_qr!(A′, τs′, merged_block_size)
+            
+            τ_offset += p.w
         end
 
         merged_block_size *= 2
     end
-
-    A
 end
+
+function apply_tsqr_left!(qr, A, τs, p::Panel)
+    m, n = size(qr)
+
+    @assert n == p.w
+
+    num_panels_vertically = ÷(m, p.h, RoundUp)
+
+    # Keep the τ offset conveniently here, cause
+    # we need that when walking up the tree flater.
+    τ_offset = 1
+
+    # First standard QR on every panel.
+    for b = OneTo(num_panels_vertically)
+        b_start = (b - 1) * p.h + 1
+        b_end = min(b_start + p.h - 1, m)
+        τs′ = view(τs, τ_offset:τ_offset+p.w-1)
+
+        qr′ = view(qr, b_start:b_end, :)
+        A′ = view(A, b_start:b_end, :)
+
+        #=@spawn=# apply_tiny_Q_left!(qr′, A′, τs′)
+
+        τ_offset += p.w
+    end
+
+    # Then we walk up the tree and do QR on upper triangular blocks.
+    tree_height = exponent_of_2_greater_than_or_equal_to(num_panels_vertically)
+
+    # we're combining two panels of height `merged_block_size`
+    merged_block_size = p.h
+
+    # we've already done the leaf nodes, so - 1.
+    for level = OneTo(tree_height)
+        num_panels_vertically = ÷(m, merged_block_size, RoundUp)
+        
+        # combine all pairs of panels.
+        for b = OneTo(num_panels_vertically ÷ 2)
+            b_start = (b - 1) * 2merged_block_size + 1
+            b_end = min(b_start + 2merged_block_size - 1, m)
+
+            qr′ = view(qr, b_start:b_end, :)
+            A′ = view(A, b_start:b_end, :)
+            τs′ = view(τs, τ_offset:τ_offset+p.w-1)
+            
+            #=@spawn=# apply_tiny_Q_with_gap_left!(qr′, A′, τs′, merged_block_size)
+            
+            τ_offset += p.w
+        end
+
+        merged_block_size *= 2
+    end
+end
+
+function apply_tsqr_right!(qr, A, τs, p::Panel)
+    m, n = size(qr)
+
+    @assert n == p.w
+
+    num_panels_vertically = ÷(m, p.h, RoundUp)
+
+    # Keep the τ offset conveniently here, cause
+    # we need that when walking up the tree flater.
+    τ_offset = 1
+
+    # First standard QR on every panel.
+    for b = OneTo(num_panels_vertically)
+        b_start = (b - 1) * p.h + 1
+        b_end = min(b_start + p.h - 1, m)
+        τs′ = view(τs, τ_offset:τ_offset+p.w-1)
+
+        qr′ = view(qr, b_start:b_end, :)
+        A′ = view(A, :, b_start:b_end)
+
+        #=@spawn=# apply_tiny_Q_right!(qr′, A′, τs′)
+
+        τ_offset += p.w
+    end
+
+    # Then we walk up the tree and do QR on upper triangular blocks.
+    tree_height = exponent_of_2_greater_than_or_equal_to(num_panels_vertically)
+
+    # we're combining two panels of height `merged_block_size`
+    merged_block_size = p.h
+
+    # we've already done the leaf nodes, so - 1.
+    for level = OneTo(tree_height)
+        num_panels_vertically = ÷(m, merged_block_size, RoundUp)
+
+        # combine all pairs of panels.
+        for b = OneTo(num_panels_vertically ÷ 2)
+            b_start = (b - 1) * 2merged_block_size + 1
+            b_end = min(b_start + 2merged_block_size - 1, m)
+
+            qr′ = view(qr, b_start:b_end, :)
+            A′ = view(A, :, b_start:b_end)
+            τs′ = view(τs, τ_offset:τ_offset+p.w-1)
+            
+            #=@spawn=# apply_tiny_Q_with_gap_right!(qr′, A′, τs′, merged_block_size)
+            
+            τ_offset += p.w
+        end
+
+        merged_block_size *= 2
+    end
+end
+
+# function cols_to_band!(A, offset_row, offset_col, block_size, τs)
+#     # The part in which we do a tall and skinny QR
+#     A_qr = view(A, offset_row:size(A, 1), offset_col:min(size(A, 2), offset_col+block_size-1))
+
+#     # The part to which we apply the tall and skinny QR from the left
+#     A_left = view(A, offset_row:size(A, 1), offset_col+block_size:size(A, 2))
+
+#     # The part to which we apply the tall and skinny QR from the right.
+#     A_right = view(A, :, offset_row:size(A, 1))
+
+#     #=@sync=# for block_start = 1:block_size:size(A_qr, 1)
+#         let
+#             block_end = min(block_start + block_size - 1, size(A_qr, 1))
+#             range = block_start:block_end
+#             A_qr′ = view(A_qr, range, :)
+#             τs′ = view(τs, range)
+#             #=@spawn=# block_qr!(A_qr′, τs′)
+#         end
+#     end
+
+#     #=@sync=# for block_start = 1:block_size:size(A_qr, 1)
+#         let
+#             block_end = min(block_start + block_size - 1, size(A_qr, 1))
+#             range = block_start:block_end
+#             A_qr′ = view(A_qr, range, :)
+#             A_left′ = view(A_left, range, :)
+#             τs′ = view(τs, range)
+#             #=@spawn=# apply_left!(A_qr′, A_left′, τs′)
+#         end
+#     end
+
+#     #=@sync=# for block_start = 1:block_size:size(A_qr, 1)
+#         let
+#             block_end = min(block_start + block_size - 1, size(A_qr, 1))
+#             range = block_start:block_end
+#             A_qr′ = view(A_qr, range, :)
+#             A_right′ = view(A_right, :, range)
+#             τs′ = view(τs, range)
+#             #=@spawn=# apply_right!(A_qr′, A_right′, τs′)
+#         end
+#     end
+
+#     # Pair-wise reduce to single QR.
+#     merged_block_size = block_size
+#     while true
+#         # If there's only one block left, stop.
+#         nblocks = ÷(size(A_qr, 1), merged_block_size, RoundUp)
+#         nblocks ≤ 1 && break
+
+#         #=@sync=# for block_start = 1:2merged_block_size:size(A_qr, 1)
+#             let
+#                 # If only a single block or less fits, stop.
+#                 block_start + merged_block_size - 1 ≥ size(A_qr, 1) && break
+    
+#                 block_end = min(block_start + 2merged_block_size - 1, size(A_qr, 1))
+#                 range = block_start:block_end
+#                 A_qr′ = view(A_qr, range, :)
+#                 τs′ = view(τs, range)
+#                 #=@spawn=# merge_block_qr!(A_qr′, τs′, merged_block_size)
+#             end
+#         end
+
+#         #=@sync=# for block_start = 1:2merged_block_size:size(A_qr, 1)
+#             let
+#                 # If only a single block or less fits, stop.
+#                 block_start + merged_block_size - 1 ≥ size(A_qr, 1) && break
+
+#                 block_end = min(block_start + 2merged_block_size - 1, size(A_qr, 1))
+#                 range = block_start:block_end
+#                 A_qr′ = view(A_qr, range, :)
+#                 A_left′ = view(A_left, range, :)
+#                 τs′ = view(τs, range)
+#                 #=@spawn=# apply_left_with_gap!(A_qr′, A_left′, τs′, merged_block_size)
+#             end
+#         end
+
+#         #=@sync=# for block_start = 1:2merged_block_size:size(A_qr, 1)
+#             let
+#                 # If only a single block or less fits, stop.
+#                 block_start + merged_block_size - 1 ≥ size(A_qr, 1) && break
+
+#                 block_end = min(block_start + 2merged_block_size - 1, size(A_qr, 1))
+#                 range = block_start:block_end
+#                 A_qr′ = view(A_qr, range, :)
+#                 A_right′ = view(A_right, :, range)
+#                 τs′ = view(τs, range)
+#                 #=@spawn=# apply_right_with_gap!(A_qr′, A_right′, τs′, merged_block_size)
+#             end
+#         end
+
+#         merged_block_size *= 2
+#     end
+
+#     A
+# end
+
+### just a simple dense QR.
 
 function block_qr!(A, τs)
     m, n = size(A)
 
-    steps = min(m, n) - 1
+    B = copy(A)
+
+    steps = min(m - 1, n)
 
     # Create a bunch of reflectors and save the norm value (τ)
     @inbounds for k = OneTo(steps)
@@ -134,29 +351,7 @@ function block_qr!(A, τs)
     return A
 end
 
-function apply_left!(qr, A, τs)
-    m, n = size(qr)
-
-    steps = min(m, n) - 1
-    
-    @inbounds for k = OneTo(steps)
-        reflectorApplyLeft!(view(A, k:size(A, 1), :), τs[k], view(qr, k:m, k))
-    end
-
-    return A
-end
-
-function apply_right!(qr, A, τs)
-    m, n = size(qr)
-
-    steps = min(m, n) - 1
-    
-    @inbounds for k = OneTo(steps)
-        reflectorApplyRight!(view(A, :, k:size(A, 2)), τs[k], view(qr, k:m, k))
-    end
-    
-    return A
-end
+### QR two stacked, upper triangular matrices. 
 
 function merge_block_qr!(A, τs, v_block_size)
     m, n = size(A)
@@ -173,7 +368,33 @@ function merge_block_qr!(A, τs, v_block_size)
     return A
 end
 
-function apply_left_with_gap!(qr, A, τs, v_block_size)
+### Applying Q from a tiny QR decomp.
+
+function apply_tiny_Q_left!(qr, A, τs)
+    m, n = size(qr)
+
+    steps = min(m - 1, n)
+    
+    @inbounds for k = OneTo(steps)
+        reflectorApplyLeft!(view(A, k:size(A, 1), :), τs[k], view(qr, k:m, k))
+    end
+
+    return A
+end
+
+function apply_tiny_Q_right!(qr, A, τs)
+    m, n = size(qr)
+
+    steps = min(m - 1, n)
+    
+    @inbounds for k = OneTo(steps)
+        reflectorApplyRight!(view(A, :, k:size(A, 2)), τs[k], view(qr, k:m, k))
+    end
+    
+    return A
+end
+
+function apply_tiny_Q_with_gap_left!(qr, A, τs, v_block_size)
     m, n = size(qr)
 
     # Create a bunch of reflectors and save the norm value (τ)
@@ -187,7 +408,7 @@ function apply_left_with_gap!(qr, A, τs, v_block_size)
     return A
 end
 
-function apply_right_with_gap!(qr, A, τs, v_block_size)
+function apply_tiny_Q_with_gap_right!(qr, A, τs, v_block_size)
     m, n = size(qr)
 
     # Create a bunch of reflectors and save the norm value (τ)
@@ -196,11 +417,12 @@ function apply_right_with_gap!(qr, A, τs, v_block_size)
         to = min(m, k + v_block_size)
         x = view(qr, k:to, k)
         reflectorApplyRight!(view(A, :, k:to), τs[k], x, gap)
-        qr[k+1:to, k] .= 0
     end
 
     return A
 end
+
+#### Computing and applying a single reflector
 
 @inline function reflector!(x::AbstractVector, gap = 1)
     @inbounds begin
