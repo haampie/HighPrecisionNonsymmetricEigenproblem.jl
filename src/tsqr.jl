5bec539158343447ccc86b908c8eb5a2e3ae1035
Harmen Stoppels <harmenstoppels@gmail.com>
2020-12-30 01:03:56 +0100

    More let blocks

--- src/tsqr.jl
+++ src/tsqr.jl
@@ -163,20 +163,21 @@ function apply_tsqr_left!(qr, A, τs, p::Panel)
 
     # we've already done the leaf nodes, so - 1.
     for level = OneTo(tree_height)
-        num_panels_vertically = ÷(m, merged_block_size, RoundUp)
+        let num_panels_vertically = ÷(m, merged_block_size, RoundUp), merged_block_size=merged_block_size
         
-        # combine all pairs of panels.
-        @sync for b = OneTo(num_panels_vertically ÷ 2)
-            b_start = (b - 1) * 2merged_block_size + 1
-            b_end = min(b_start + 2merged_block_size - 1, m)
+            # combine all pairs of panels.
+            @sync for b = OneTo(num_panels_vertically ÷ 2)
+                b_start = (b - 1) * 2merged_block_size + 1
+                b_end = min(b_start + 2merged_block_size - 1, m)
 
-            qr′ = view(qr, b_start:b_end, :)
-            A′ = view(A, b_start:b_end, :)
-            τs′ = view(τs, τ_offset:τ_offset+p.w-1)
-            
-            @spawn apply_tiny_Q_with_gap_left!(qr′, A′, τs′, merged_block_size)
-            
-            τ_offset += p.w
+                qr′ = view(qr, b_start:b_end, :)
+                A′ = view(A, b_start:b_end, :)
+                τs′ = view(τs, τ_offset:τ_offset+p.w-1)
+                
+                @spawn apply_tiny_Q_with_gap_left!(qr′, A′, τs′, merged_block_size)
+                
+                τ_offset += p.w
+            end
         end
 
         merged_block_size *= 2
@@ -216,20 +217,21 @@ function apply_tsqr_right!(qr, A, τs, p::Panel)
 
     # we've already done the leaf nodes, so - 1.
     for level = OneTo(tree_height)
-        num_panels_vertically = ÷(m, merged_block_size, RoundUp)
+        let num_panels_vertically = ÷(m, merged_block_size, RoundUp), merged_block_size=merged_block_size
 
-        # combine all pairs of panels.
-        @sync for b = OneTo(num_panels_vertically ÷ 2)
-            b_start = (b - 1) * 2merged_block_size + 1
-            b_end = min(b_start + 2merged_block_size - 1, m)
+            # combine all pairs of panels.
+            @sync for b = OneTo(num_panels_vertically ÷ 2)
+                b_start = (b - 1) * 2merged_block_size + 1
+                b_end = min(b_start + 2merged_block_size - 1, m)
 
-            qr′ = view(qr, b_start:b_end, :)
-            A′ = view(A, :, b_start:b_end)
-            τs′ = view(τs, τ_offset:τ_offset+p.w-1)
-            
-            @spawn apply_tiny_Q_with_gap_right!(qr′, A′, τs′, merged_block_size)
-            
-            τ_offset += p.w
+                qr′ = view(qr, b_start:b_end, :)
+                A′ = view(A, :, b_start:b_end)
+                τs′ = view(τs, τ_offset:τ_offset+p.w-1)
+                
+                @spawn apply_tiny_Q_with_gap_right!(qr′, A′, τs′, merged_block_size)
+                
+                τ_offset += p.w
+            end
         end
 
         merged_block_size *= 2
